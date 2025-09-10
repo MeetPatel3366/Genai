04c2fe715d2ef5dc5dab174f3dde3defa4ebe334
Meet Patel <meet3366patel@gmail.com>
2025-09-10 22:17:39 +0530

    Structured Output format to return valid JSON structure

--- app.js
+++ app.js
@@ -3,26 +3,15 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
   const completion = await groq.chat.completions.create({
-    temperature: 1,
-    // top_p:0.2,
-
-    /**
-     * 1. item 1
-     * 2. item 2
-     * ....
-     * 10 item 10
-     * 11 stop here
-     * **/
-    // stop:'11',
-    stop: "ga", //Negative
-    // max_completion_tokens: 1000,
-    frequency_penalty: 1,
-    presence_penalty: 1,
+    temperature: 1,  
+    response_format:{'type':'json_object'}  ,
     model: "llama-3.3-70b-versatile",
     messages: [
       {
         role: "system",
-        content: `You are Jarvis,a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. Output must be a single word.`,
+        content: `You are Jarvis,a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. You must return the result in valid JSON structure.
+        Example: {"sentiment": "Negative"}
+        `,
       },
       {
         role: "user",
@@ -31,7 +20,7 @@ async function main() {
       },
     ],
   });
-  console.log(completion.choices[0].message.content);
+  console.log(JSON.parse(completion.choices[0].message.content));
 }
 
 main();
