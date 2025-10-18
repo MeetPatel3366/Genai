4c70578e06a02c67c61b309d2b13b04efc486683
Meet Patel <meet3366patel@gmail.com>
2025-10-18 18:31:27 +0530

    LLM  Settings

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
   const completion = await groq.chat.completions.create({
+    temperature: 1,
+    // top_p:0.2,
+    stop: "ga", //negative
+    max_completion_tokens: 1000,
+    // frequency_penalty:2,
+    presence_penalty: 2,
     model: "llama-3.3-70b-versatile",
     messages: [
       {
