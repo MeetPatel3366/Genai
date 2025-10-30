01f3566a83ba0ad7f291fc9ceb08cbd38824c945
Meet Patel <meet3366patel@gmail.com>
2025-10-30 20:44:32 +0530

    Tool Calling: improve tool call handling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ async function main() {
     {
       role: "system",
       content: `You are a helpful assistant that can answer questions.
-You have access to a tool called "webSerach" which lets you search the internet for up-to-date information.`,
+You have access to a tool called "webSearch" which lets you search the internet for up-to-date information.`,
     },
     {
       role: "user",
@@ -17,107 +17,77 @@ You have access to a tool called "webSerach" which lets you search the internet 
     },
   ];
 
-  const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSerach",
-          description:
-            "Search the latest information and realtime data on the internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The serach query to perform serach on.",
+  while (true) {
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      temperature: 0,
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description:
+              "Search the latest information and realtime data on the internet.",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "The serach query to perform serach on.",
+                },
               },
+              required: ["query"],
             },
-            required: ["query"],
           },
         },
-      },
-    ],
-    tool_choice: "auto",
-  });
+      ],
+      tool_choice: "auto",
+    });
 
-  console.log("3----> ", completion.choices[0].message);
+    // console.log("3----> ", completion.choices[0].message);
 
-  messages.push(completion.choices[0].message);
-  const toolCalls = completion.choices[0].message.tool_calls;
+    messages.push(completion.choices[0].message);
+    const toolCalls = completion.choices[0].message.tool_calls;
 
-  if (!toolCalls) {
-    console.log(`Assistant: ${completion.choices[0].message.content}`);
-    return;
-  }
+    if (!toolCalls) {
+      console.log(`Assistant: ${completion.choices[0].message.content}`);
+      break;
+    }
 
-  for (const tool of toolCalls) {
-    console.log("tool: ", tool);
-    const functionName = tool.function.name;
-    const functionArguments = tool.function.arguments;
+    for (const tool of toolCalls) {
+      // console.log("tool: ", tool);
+      const functionName = tool.function.name;
+      const functionArguments = tool.function.arguments;
 
-    if (functionName == "webSerach") {
-      const toolResult = await webSerach(JSON.parse(functionArguments));
-      console.log(toolResult);
+      if (functionName == "webSearch") {
+        const toolResult = await webSearch(JSON.parse(functionArguments));
+        // console.log(toolResult);
 
-      messages.push({
-        tool_call_id: tool.id,
-        role: "tool",
-        name: functionName,
-        content: toolResult,
-      });
+        messages.push({
+          tool_call_id: tool.id,
+          role: "tool",
+          name: functionName,
+          content: toolResult,
+        });
+      }
     }
   }
-
-  const completion2 = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSerach",
-          description:
-            "Search the latest information and realtime data on the internet.",
-          parameters: {
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The serach query to perform serach on.",
-              },
-            },
-            required: ["query"],
-          },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
-
-  console.log("------------------------------");
-  console.log(
-    "final result: ",
-    JSON.stringify(completion2.choices[0].message, null, 2)
-  );
 }
 main();
 
-async function webSerach({ query }) {
+async function webSearch({ query }) {
   //Here we will do tavily api call
-  console.log("Calling web search : ", query);
+  console.log("Calling web search ...");
+  // console.log("Calling web search : ", query);
 
   const response = await tvly.search(query);
-  console.log("response: ", response);
+  // console.log("response: ", response);
 
   const finalResult = response.results
     .map((result) => result.content)
     .join("\n\n");
-  console.log("finalResult : ", finalResult);
+  // console.log("finalResult : ", finalResult);
 
   return finalResult;
 }
