2ddfd67698028b94efc938d71e6af5dee6130b7d
MeetPatel3366 <meet3366patel@gmail.com>
2026-01-11 17:39:52 +0530

    Tool calling practical: setup

--- app.js
+++ app.js
@@ -4,63 +4,73 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function getGroqChatCompletion() {
   return groq.chat.completions.create({
-    temperature: 1,
-    top_p: 0.2,
-    /**
-     *  1. item 1
-        2. item 2
-        .....
-        10. item 10
-        11. item 11
-     */
-    // stop:"11.",
-    // stop: "ga", //Negative
-    max_completion_tokens: 100,
-    frequency_penalty: 1.0,
-    presence_penalty: 1.0,
+    temperature: 0,
     model: "llama-3.3-70b-versatile",
-    response_format: { type: "json_object" },
     messages: [
       {
         role: "system",
-        content: `You are an interview grader assistant. Your task is to generate candidate evaluation score. Output must be following JSON structure:
-        { 
-          "confidence": number (1-10 scale),
-          "accuracy": number (1-10 scale),
-          "pass": boolean (true or false)
-        }
-        The response must:
-          1. Include All fields shown above
-          2. Use only the exact field names shown
-          3. Follow the exact data types specified
-          4. Contain only the JSON object and nothing else  
-        `,
+        content: `You are s samrt personal assistant who answers the asked questions.
+        You have access to following tools:
+        1. searchWeb({query}: {query:string}) //Search the latest information and realtime data on the internet`,
       },
       {
         role: "user",
-        content: `
-          Q: What does === do in JavaScript?
-          A: It checks strict equality without type conversion.
-
-          Q: How do you declare a variable in JavaScript?
-          A: Use let, const, or var followed by the variable name.
-
-          Q: What is a JavaScript array?
-          A: It's a list-like object used to store multiple values in a single variable.
-
-          Q: How do you create a function in JavaScript?
-          A: Use the function keyword followed by a name and parentheses.
-        `,
+        content: `What is the current weather in Mumbai?`,
       },
     ],
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "Search the latest information and realtime data on the internet",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform search on.",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
   });
 }
 
 async function main() {
   const chatCompletion = await getGroqChatCompletion();
 
-  console.log(chatCompletion?.choices[0]?.message.content);
-  console.log(JSON.parse(chatCompletion?.choices[0]?.message.content));
+  // console.log(chatCompletion?.choices[0]?.message);
+  // console.log(JSON.stringify(chatCompletion?.choices[0]?.message,null,2));
+
+  const toolCalls = chatCompletion.choices[0].message.tool_calls;
+
+  if (!toolCalls) {
+    console.log(`Assistant: ${chatCompletion?.choices[0]?.message.content}`);
+    return;
+  }
+
+  for (const tool of toolCalls) {
+    console.log(tool);
+    const functionName = tool.function.name;
+    const functionArguments = tool.function.arguments;
+
+    if (functionName == "webSearch") {
+      const toolResult = await webSearch(JSON.parse(functionArguments));
+      console.log("Tool result: ", toolResult);
+    }
+  }
 }
 
 main();
+
+async function webSearch({ query }) {
+  console.log("calling webSearch");
+
+  return "Mumbai weather is 28° C";
+}
